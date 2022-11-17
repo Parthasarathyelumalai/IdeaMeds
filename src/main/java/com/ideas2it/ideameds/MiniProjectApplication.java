7acea926b93d529a05f02fefb0da36ff6b861cbb
Parthasarathyelumalai <parthasarathy.elumalai@ideas2it.com>
2022-11-17 11:53:06 +0530

    First commit

---
+++ src/main/java/com/ideas2it/ideameds/MiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ideas2it.ideameds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjectApplication.class, args);
+	}
+
+}
