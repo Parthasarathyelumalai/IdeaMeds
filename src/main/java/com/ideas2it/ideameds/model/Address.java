a878cb5796e4f33d3ecef26b8ab245e73cbf6700
Parthasarathyelumalai <parthasarathy.elumalai@ideas2it.com>
2022-11-17 18:35:02 +0530

    Add databases schema

--- src/main/java/com/ideas2it/ideameds/model/Address.java
+++ src/main/java/com/ideas2it/ideameds/model/Address.java
@@ -3,6 +3,11 @@ package com.ideas2it.ideameds.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Simple JavaBean domain object representing an Address.
  *
@@ -10,9 +15,12 @@ import lombok.NoArgsConstructor;
  * @version - 1.0
  * @since - 2022-11-17
  */
+@Entity
 @Data
 @NoArgsConstructor
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
     private String plotNumber;
     private String streetName;
--- src/main/java/com/ideas2it/ideameds/model/UserMedicine.java
+++ src/main/java/com/ideas2it/ideameds/model/UserMedicine.java
@@ -3,6 +3,8 @@ package com.ideas2it.ideameds.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
 /**
  * Simple JavaBean domain object representing a User Medicine.
  *
@@ -10,10 +12,16 @@ import lombok.NoArgsConstructor;
  * @version - 1.0
  * @since - 2022-11-17
  */
+@Entity
 @Data
 @NoArgsConstructor
 public class UserMedicine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userMedicineId;
     private int quantity;
     private String dosage;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
