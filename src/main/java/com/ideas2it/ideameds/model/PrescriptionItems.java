eb7a0af72280ac26632f18a429601436885dfdb4
Nithish K <nithish.karthikeyan@ideas2it.com>
2022-11-17 18:30:34 +0530

    Added database schema to model prescription

--- src/main/java/com/ideas2it/ideameds/model/PrescriptionItems.java
+++ src/main/java/com/ideas2it/ideameds/model/PrescriptionItems.java
@@ -3,6 +3,14 @@ package com.ideas2it.ideameds.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 /**
  * Simple JavaBean domain object representing Prescription Items
  * @author - Nithish K
@@ -11,10 +19,20 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
+@Entity
 public class PrescriptionItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long prescriptionItemsId;
     private int quantity;
     private int dosage;
     private String medicine_type;
+
+    @ManyToOne
+    @JoinColumn(name="prescription_id")
     private Prescription prescription;
+
+    @OneToOne
+    @JoinColumn(name="medicine_id")
+    private Medicine medicine;
 }
--- src/main/java/com/ideas2it/ideameds/model/User.java
+++ src/main/java/com/ideas2it/ideameds/model/User.java
@@ -1,6 +1,5 @@
 package com.ideas2it.ideameds.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
