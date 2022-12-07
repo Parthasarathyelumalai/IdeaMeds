c7ed39e01f565880a9ddb660d15483cbd638dd1a
Nithish K <nithish.karthikeyan@ideas2it.com>
2022-12-07 15:35:00 +0530

    Deleted duplicated line

--- src/main/java/com/ideas2it/ideameds/security/SecurityConfig.java
+++ src/main/java/com/ideas2it/ideameds/security/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .authorizeHttpRequests()
                 .antMatchers("/v2/api-docs",
                         "/swagger-resources",
